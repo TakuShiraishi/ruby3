595e2b72f53d359f65687f3a03a89181dfe911fa
白石拓 <shiraishitaku@shiraishitakunoMacBook-Pro.local>
2023-01-08 17:16:44 +0900

    プロゲイト練習

---
+++ menu3.rb
@@ -0,0 +1,8 @@
+require "./menu"
+
+menu1 = Menu.new(name: "ピザ", price: 800)
+menu2 = Menu.new(name: "寿司", price: 1000)
+menu3 = Menu.new(name: "コーラ", price: 500)
+menu4 = Menu.new(name: "お茶", price: 300)
+
+
