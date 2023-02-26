11d52d08e36d4ef9ee11b0dab64d5129aaf3b771
白石拓 <s.taku0718@gmail.com>
2023-02-26 16:40:07 +0900

    progate 3

---
+++ java8/main.java
@@ -0,0 +1,27 @@
+package java8;
+
+public class main {
+  public static void main(String[] args) {
+    
+    String name = "にんじゃわんこ";
+    
+    
+    int age = 14;
+    
+    double height = 170.2;
+    
+    
+    double weight = 66.8;
+    
+    
+    double bmi = weight / height / height;
+    
+    
+    System.out.println("名前は" + name +"です");
+    System.out.println("年齢は" + age + "歳です");
+    System.out.println("身長は" + height +"mです");
+    System.out.println("体重は" + weight +"kgです");
+    System.out.println("BMIは" + bmi + "です");
+    
+  }
+}
